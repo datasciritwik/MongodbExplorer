67b18800cdb940c0b49fe6f969f5d64112456dc1
Ritwik Singh <97968834+datasciritwik@users.noreply.github.com>
2025-12-10 18:02:03 +0530

    ...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 import json
 
-st.set_page_config(page_title="MongoDB Explorer", page_icon="🧭")
+st.set_page_config(page_title="MongoDB Explorer", page_icon="🧭", layout="centered")
 
 # ---------------- SIDEBAR ----------------
 st.sidebar.header("⚙️ Configuration")
